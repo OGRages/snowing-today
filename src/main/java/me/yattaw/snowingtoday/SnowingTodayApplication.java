bfede51f53ac501897c34ea4793f443a19915a7d
Michael <whitefoxpvp@gmail.com>
2022-12-18 22:58:28 -0500

    Initial commit

---
+++ src/main/java/me/yattaw/snowingtoday/SnowingTodayApplication.java
@@ -0,0 +1,13 @@
+package me.yattaw.snowingtoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowingTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowingTodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yattaw/snowingtoday/SnowingTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yattaw.snowingtoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowingTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
