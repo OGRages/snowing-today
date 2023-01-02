fb4457756f710375718f66d4b40e125867f449b9
Michael <whitefoxpvp@gmail.com>
2023-01-01 21:25:43 -0500

    Created configuration for CORS

--- src/main/java/me/yattaw/snowingtoday/SnowingTodayApplication.java
+++ src/main/java/me/yattaw/snowingtoday/SnowingTodayApplication.java
@@ -2,14 +2,29 @@ package me.yattaw.snowingtoday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
 public class SnowingTodayApplication {
 
 	public static final String API_KEY = System.getenv("WEATHER_API_KEY");
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnowingTodayApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+			}
+		};
 	}
 
 }
